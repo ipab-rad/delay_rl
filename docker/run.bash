d07eb9dbdeb0f97b64ad62a51477cb98fd1ef27e
Simon Smith <artificialsimon@ed.ac.uk>
2019-06-10 16:39:14 +0100

    changes

--- docker/run.bash
+++ docker/run.bash
@@ -1,7 +1,8 @@
 #!/bin/bash
-nvidia-docker run -ti --rm --entrypoint /bin/bash --name delay_sensor_simon \
+nvidia-docker run -ti --entrypoint /bin/bash --name delay_sensor_simon \
 			 --group-add sudo \
 			 -v /home/artificialsimon/artificial/projects/rl_random_agents_time_delay/src/todor_mujoco_docker/docker-cuda-gym/tensorflow/mujoco:/root/.mujoco \
 			 -v /home/artificialsimon/artificial/projects/rl_random_agents_time_delay:/root \
-			 ta/gym 
+			 delay_rl
 #	 		 --user $(id -u):$(id -g) \
+# --rm
